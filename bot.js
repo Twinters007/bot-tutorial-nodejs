ec5ddb9cb92691b9c57f7501b99dcbbd47bb93c6
Tim Winters <twinters007@gmail.com>
2016-09-16 15:42:31 -0400

    Added ;

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
       //botRegex = /^\/cool guy$/;
-  console.log(request)
+  console.log(request);
   if(request.text && (request.text.toLowerCase().indexOf('c')>-1 || request.text.toLowerCase().indexOf('mariah')>-1)) {//botRegex.test(request.text))
 
     this.res.writeHead(200);
